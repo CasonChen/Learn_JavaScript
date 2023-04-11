ae792523f7372ba57251cf502dd4626965ae7873
love coding <casonchen1125@163.com>
2023-04-11 21:44:11 +0800

    Day04

---
+++ Day04/code block/code_block.js
@@ -0,0 +1,13 @@
+/*
+代码块
+    靠一个{}围起来
+js中的代码块只具有分组的作用，没有其他的用途，代码块的内容，在外部是完全可见的
+*/
+{
+    var a = 10;
+    alert("hello");
+    console.log("hello");
+    document.write("hello");
+}
+
+console.log(a);
